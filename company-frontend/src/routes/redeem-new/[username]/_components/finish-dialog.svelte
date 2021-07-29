3871da42e8551ccb4e1f797fb91712ad2f016b07
Levente Krizsan <lewalevi@gmail.com>
2021-07-29 21:17:34 +0200

    feat(redeem): add finish and reward dialog mockups

---
+++ company-frontend/src/routes/redeem-new/[username]/_components/finish-dialog.svelte
@@ -0,0 +1,30 @@
+<script>
+    import { Dialog, Modal, Button } from 'attractions';
+
+    export function open(discountedProductsToDisplay) {
+        discountedProducts = discountedProductsToDisplay;
+        modalOpen = true;
+    }
+
+    export function close() {
+        discountedProducts = null;
+        modalOpen = false;
+    }
+
+    let modalOpen = false;
+    let discountedProducts;
+</script>
+
+<Modal bind:open={modalOpen} noClickaway>
+    <Dialog title="Discounted Items">
+        {#each discountedProducts as product}
+            <div>{product.name}</div>
+            <div>{product.originalPrice}</div>
+            <div>{product.discountedPrice}</div>
+        {/each}
+    </Dialog>
+</Modal>
+
+<style lang="scss">
+
+</style>
--- company-frontend/src/routes/redeem-new/[username]/_components/reward-dialog.svelte
+++ company-frontend/src/routes/redeem-new/[username]/_components/reward-dialog.svelte
@@ -1,14 +1,28 @@
 <script>
     import { Dialog, Modal, Button } from 'attractions';
 
-    export let coupon;
+    export function open(couponToDisplay) {
+        coupon = couponToDisplay;
+        modalOpen = true;
+    }
+
+    export function close() {
+        coupon = null;
+        modalOpen = false;
+    }
 
     let modalOpen = false;
+    let coupon;
 </script>
 
 <Modal bind:open={modalOpen} noClickaway>
     <Dialog title={`Choose Reward for ${coupon.name}`}>
-
+        {#each coupon.rewards as reward}
+            <div>{reward.name}</div>
+            <div>{reward.amount}</div>
+            <div>{reward.originalPrice}</div>
+            <div>{reward.discountedPrice}</div>
+        {/each}
     </Dialog>
 </Modal>
 
